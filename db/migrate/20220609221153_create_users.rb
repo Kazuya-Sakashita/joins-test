35268f9ccb8499bdd99783db5016e4a700053427
Kazuya-Sakashita <kz0508@gmail.com>
2022-06-10 07:33:41 +0900

    db migrate

--- db/migrate/20220609221153_create_users.rb
+++ db/migrate/20220609221153_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       t.string :user_name
-      t.indeger :gender
+      t.integer :gender
 
       t.timestamps
     end
